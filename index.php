2dbc93f77d83bfeb361c1e5e81faeac8a0cda9b2
Legatus727 <86779745+Legatus727@users.noreply.github.com>
2023-03-16 21:58:29 -0400

    Home Page and Register Page

--- index.php
+++ index.php
@@ -1,59 +1,66 @@
 <!DOCTYPE html>
 <head>
     <title>Avenger's Training School</title>
+    <style>
+        h1 {
+            text-align: center;
+        }
+        section {
+            text-align: center;
+        }
+        label {
+             display: inline-block;
+             width: 10%;
+             text-align: right;
+        }
+    </style>
 </head>
 <body>
-
-<?php
-    include("indexPHP.php");
-?>
 
 <h1>Avenger's School</h1>
 
 <section>
+    <a href="/Databse-II/register.php">Register New Account</a>
+    <br><br>
     <form action="" method="post">
         <label for="email">Email: </label>
         <input type="text" id="email" name="email">
-        <br>
+        <br><br>
         <label for="password">Password: </label>
-        <input type="password" id="password" name="password">
-        <br>
-        <input type="login" name="login" value="Login">
+        <input type="text" id="password" name="password">
+        <br><br>
+        <input type="submit" name="submit" value="Login">
     </form>
 </section>
 
+<?php
+$dbConnection = mysqli_connect("localhost", "root", "", "DB2");
+if (!$dbConnection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
-<article>
-    <form action="" method="post">
-        <label for="AcctId">Choose Account Type: </label>
-            <select name="AcctId" id="AcctId">
-                <option value="admin">Admin</option>
-                <option value="student">Student</option>
-                <option value="parent">Parent</option>
-            </select>
-        <br><br>
-        <label for="UID">Enter UID: </label>
-        <input type="text" id="UID" name="UID">
-        <br><br>
-        <label for="Action">Choose Action: </label>
-        <select name="Action" id="Action">
-            <option value="Admin - Create Material">Admin - Create Material</option>
-            <option value="Admin - Create Meeting">Admin - Create Meeting</option>
-            <option value="Admin - Modify Material">Admin - Modify Material</option>
-        </select>
-        <br><br>
+if(isset($_POST['submit'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-        <input type="submit" name="submit" value="Save">
-    </form>
+    $query = 'select password from users where email = "' . $email . '"';
+    $result = mysqli_query($dbConnection, $query);
 
-    <form method="post">
-        <input type="submit" name="TestRetrieveStudents"
-               value="TestRetrieveStudents"/>
-
-        <input type="submit" name="TestRetrieveAdmin"
-               value="TestRetrieveAdmin"/>
-    </form>
-</article>
+    if (mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+            $dbPassword = $row["password"];
+            if ($password == $dbPassword) {
+                // Go to page based on user type (i.e. Student, Admin, Parents)
+                echo "Jumping to user specific page";
+            } else {
+                echo "<br>Password invalid.";
+            }
+        }
+    } else {
+        echo "<br>Email invalid. No account found.";
+    }
+}
+?>
 
 </body>
 </html>
