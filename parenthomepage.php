f293936395b87fd14555f7efd8ab8f15f2b26f8d
saltyseas69 <116859227+saltyseas69@users.noreply.github.com>
2023-03-20 14:31:42 -0400

    Update parenthomepage.php

--- parenthomepage.php
+++ parenthomepage.php
@@ -7,6 +7,20 @@
     <body>
         <h1>Avengers Parents Initiative</h1>
 
-        <h3>Students</h3>
+        <h3>Child Meeting Info</h3>
+        <?php
+            $dbConnection = mysqli_connect("localhost", "root", "", "DB2");
+            if (!$dbConnection) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+            
+            $student = array(students, meetings, groups);
+            list($a, $b, $c) = $student;
+            echo "Student Name: $a";
+            echo "<br>";
+            echo "Current Meetings: $b";
+            echo "<br>";
+            echo "Current Groups: $c";
+        ?>
     </body>
 </html>
