e538e1f44e9d29bb2dace5c015fe2c149d2a71a3
saltyseas69 <116859227+saltyseas69@users.noreply.github.com>
2023-03-20 14:04:24 -0400

    Create parenthomepage.php

---
+++ parenthomepage.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Parent Homepage</title>
+    </head>
+
+    <body>
+        <h1>Avengers Parents Initiative</h1>
+
+        <h3>Students</h3>
+    </body>
+</html>
