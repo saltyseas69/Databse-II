33353a4cb7f117249eab4c69060f7463e31d1b2b
saltyseas69 <116859227+saltyseas69@users.noreply.github.com>
2023-03-20 20:07:10 -0400

    Update parenthomepage.php

--- parenthomepage.php
+++ parenthomepage.php
@@ -27,14 +27,29 @@
                 die("Connection failed: " . mysqli_connect_error());
             }
             
-            $sql = "SELECT * FROM students";
+            $childof = "SELECT student_id FROM child_of WHERE parent_id = 1";
+            $childof_result = $dbConnection->query($childof);
+                while($row = $childof_result->fetch_assoc()){
+                    echo "<br> Child ID: ". $row["student_id"];
+                }
+
+            $studentname = "SELECT * FROM users WHERE id = 7";
+            $sname_result = $dbConnection->query($studentname);
+                while($row = $sname_result->fetch_assoc()){
+                    echo "<br> Child Name: ". $row["name"]. "<br>";
+                }
+            
+            
+            
+            
+            $sql = "SELECT name FROM groups";
 
             $result = $dbConnection->query($sql);
 
             if ($result->num_rows > 0) {
                 // output data of each row
                 while($row = $result->fetch_assoc()) {
-                    echo "<br> Student ID: ". $row["student_id"]. "<br>";
+                    echo "<br> Group Name: ". $row["name"]. "<br>";
                 }
             } 
             else{
