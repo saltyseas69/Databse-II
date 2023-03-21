1a7926f107cbad13fe5d528aa6797d6beb55df98
saltyseas69 <116859227+saltyseas69@users.noreply.github.com>
2023-03-21 12:58:02 -0400

    Update parenthomepage.php

--- Parent/parenthomepage.php
+++ Parent/parenthomepage.php
@@ -21,16 +21,30 @@
         <h1>Avengers Parents Initiative</h1>
         <?php
             session_start();
-            
             $dbConnection = mysqli_connect("localhost", "root", "", "DB2");
             if (!$dbConnection) {
                 die("Connection failed: " . mysqli_connect_error());
             }
 
-            $parent = "SELECT name FROM users WHERE id = " . $_SESSION['sessionID'];
-            $parent_result = $dbConnection->query($parent);
-            while($row = $parent_result->fetch_assoc()){
-                echo "Welcome " . $row["name"];
+            $query = "select * from users where id = " . $_SESSION['sessionID'];
+            $result = mysqli_query($dbConnection, $query);
+
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $currID = $row['id'];
+                    $currEmail = $row['email'];
+                    $currPassword = $row['password'];
+                    $currName = $row['name'];
+                    $currPhone = $row['phone'];
+
+                    echo "ID: $currID<br>" .
+                        "Email: $currEmail<br>" .
+                        "Password: $currPassword<br>" .
+                        "Name: $currName<br>" .
+                        "Phone: $currPhone<br>";
+                }
+            } else {
+                echo "Unexpected Error: Cannot retrieve account information";
             }
         ?>
         
@@ -40,24 +54,24 @@
             
             //select the child of the parent that logged in
             $childof = "SELECT student_id FROM child_of WHERE parent_id = " . $_SESSION['sessionID'];
-            
             //the variable defined in the last line is used to access the db
             $childof_result = $dbConnection->query($childof);
-
             //now fetch all the data while the rows are not empty
                 while($row = $childof_result->fetch_assoc()){
-                    $sname = $row["student_id"];
+                    $child_name = $row["student_id"];
                     echo "<br> Child ID: ". $row["student_id"];
                 }
             
             //select the child name using the result of previous query
-            $studentname = "SELECT * FROM users WHERE id = " . $sname;
+            $studentname = "SELECT * FROM users WHERE id = " . $child_name;
             $sname_result = $dbConnection->query($studentname);
                 while($row = $sname_result->fetch_assoc()){
                     echo "<br> Child Name: ". $row["name"]. "<br>";
                 }
         ?>
+
         <h3>Current Meetings</h3>
+        
         <?php
 
             $current_meeting = "SELECT meeting_name\n" 
@@ -73,18 +87,38 @@
             else{
                 echo "0 results";
             }
-
-            $dbConnection->close();
         ?>
-        <h3>Add Meetings</h3>
+
+        <h3>Add Child to Meetings</h3>
+        
         <form method="post">
-        <input type="submit" name="submit" value="Add Meeting"/>
+        <input type="submit" name="submit" value="Add"/>
         </form>
 
         <?php
-            if(isset($_POST['submit'])){
-                $sql = "INSERT INTO enroll";
-                $result = $dbConnection->query($sql);
+            $query = 'select * from meetings';
+            $result = mysqli_query($dbConnection, $query);
+        
+            if (mysqli_num_rows($result) > 0) {
+        
+                while($row = mysqli_fetch_assoc($result)) {
+                    $meetingName = $row['meeting_name'];
+                    $date = $row['date'];
+                    $timeSlot = $row['time_slot_id'];
+                    $capacity = $row['capacity'];
+                    $groupId = $row['group_id'];
+                    $announcement = $row['announcement'];
+        
+                    echo
+                        "Meeting Name: $meetingName<br>" .
+                        "Date: $date<br>" .
+                        "Time Slot: $timeSlot<br>" .
+                        "Capacity: $capacity<br>" .
+                        "GroupID: $groupId<br>" .
+                        "Announcement: $announcement <br>---------------------------------------------------------<br>";
+                }
+            }else{
+                echo "0 results";
             }
         ?>
     </body>
