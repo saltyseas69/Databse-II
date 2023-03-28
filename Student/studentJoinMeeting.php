3a7c83f256d24055a87b3c9152125d84b4fdb2ce
saltyseas69 <116859227+saltyseas69@users.noreply.github.com>
2023-03-27 23:27:59 -0400

    Update studentJoinMeeting.php

--- Student/studentJoinMeeting.php
+++ Student/studentJoinMeeting.php
@@ -15,7 +15,7 @@
     <a href="./studentMeetingViewer.php">Home</a> |
     <a href="./studentAccount.php">Account</a> |
     <a href="./selectGrade.php">Select Grade</a> |
-    <a href="./studentJoinMeeting.php">Join</a> |
+    <a href="./studentJoinMeeting.php">Join Meeting</a> |
     <a href="./ta.php">Teacher Assistant</a> |
     <a href="../index.php">Logout</a>
 </nav>
@@ -49,10 +49,9 @@
 <section>
     <br><br>
     <form action="" method="post">
-
         <label for="group">Select Meeting to Join: </label>
         <form action="" method="post">
-        <input type="text" id="meeting" name="meeting">
+        <input type="text" id="joinmeet" name="joinmeet">
         <br><br>
         <input type="submit" name="join" value="Join">
         <br><br>
@@ -65,7 +64,7 @@
 
         <label for="group">Select Meeting to Delete: </label>
         <form action="" method="post">
-        <input type="text" id="meeting" name="meeting">
+        <input type="text" id="deletemeet" name="deletemeet">
         <br><br>
         <input type="submit" name="delete" value="Delete">
         <br><br>
@@ -73,16 +72,22 @@
 </section>
 
 <?php
-$query = 'select * from meetings';
-$studentQuery = 'SELECT student_id
-                FROM enroll, meetings
-                WHERE enroll.meeting_id =  meetings.meeting_id';
-            $studentResult = mysqli_query($dbConnection, $studentQuery);
-$studentQuery1 = 'SELECT name, email
-                FROM users, students
-                WHERE users.id = '. $studentResult  ;
-                $studentResult1 = mysqli_query($dbConnection, $studentQuery1);
-            $result = mysqli_query($dbConnection, $query);
+        function redirect($url) {
+            header('Location: '.$url);
+        }
+
+// $studentQuery = 'SELECT student_id
+//                  FROM enroll, meetings
+//                  WHERE enroll.meeting_id =  meetings.meeting_id';
+//                  //GROUP BY meeting_id
+//             $studentResult = mysqli_query($dbConnection, $studentQuery);
+
+// $studentQuery1 = 'SELECT name, email
+//                 FROM users, students
+//                 WHERE users.id = students.'.$studentResult;
+//                 $studentResult1 = mysqli_query($dbConnection, $studentQuery1);
+                $query = 'select * from meetings';
+                $result = mysqli_query($dbConnection, $query);
             
             
             if (mysqli_num_rows($result) > 0) {
@@ -95,8 +100,7 @@ $studentQuery1 = 'SELECT name, email
                     $capacity = $row['capacity'];
                     $groupID = $row['group_id'];
                     $announcement = $row['announcement'];
-                    $studentName = $row['name'];
-                    $studentEmail = $row['email'];
+
         
                     echo"Meeting Name : $meetingName<br>".
                         "Meeting ID : $meetingID<br>" .
@@ -104,48 +108,161 @@ $studentQuery1 = 'SELECT name, email
                         "Time: $time<br>" .
                         "Capacity: $capacity<br>" .
                         "Group ID: $groupID<br>" .
-                        "Announcement: $announcement<br>" .
-                        "Students in Meeting :  $studentName ". 
+                        "Announcement: $announcement<br>" . 
 
                         "<br>---------------------------------------------------------<br>";
                 }
+                // while($row = mysqli_fetch_assoc($studentResult1)){
+                //     $studentName = $row['name'];
+                //     $studentEmail = $row['email'];
+
+                //     echo"Student Name: $studentName<br>";
+                // }
+
             }else{
                 echo "0 results";
             }
-
             if(isset($_POST['join'])) {
-                $meeting_id = $_POST['meeting'];
-                if(empty($meeting_id)){
-                    echo "Please enter meeting ID";
-                }
-                else if($meeting_id = $meetingID){
-                    $createQuery = 'insert into enroll values (' . 
-                        $meeting_id . ', '. $_SESSION['sessionID']. ')'; 
-                        $addresult = mysqli_query($dbConnection, $createQuery);
-                        echo "Meeting has been successfully added.";
+                $meetingID = $_POST['joinmeet'];
+                $studentID = $_SESSION['sessionID'];
+                if (empty($meetingID)) {
+                    echo "<br><br>Input valid Meeting ID";
+                } else {
             
-                }
-                else {
-                    echo "No meetings added"; 
+                    // Query meeting desired for its associate group
+                    $validateGradeQuery = 'select * from meetings where meeting_id = ' . $meetingID;
+                    try {
+                        $validationResults = mysqli_query($dbConnection, $validateGradeQuery);
+                    } catch (mysqli_sql_exception $e) {
+                        echo $e;
+                        die();
+                    }
+                    $meetingCheckForValidation = mysqli_fetch_assoc($validationResults);
+                    $groupIDToCheckForValidation =  $meetingCheckForValidation['group_id'];
+            
+                    // Query associated group for its grade requirement
+                    $groupIDValidationQuery = 'select grade_req from groups where group_id = ' . $groupIDToCheckForValidation;
+                    try {
+                        $groupIDValidationResult = mysqli_query($dbConnection, $groupIDValidationQuery);
+                    } catch (mysqli_sql_exception $e) {
+                        echo $e;
+                        die();
+                    }
+            
+                    $gradeReqRow = mysqli_fetch_assoc($groupIDValidationResult);
+                    $gradeReq = $gradeReqRow['grade_req'];
+            
+                    // Query student for their grade
+                    $studentGradeQuery = 'select grade from students where student_id = ' . $studentID;
+                    try {
+                        $studentGradeResult = mysqli_query($dbConnection, $studentGradeQuery);
+                    } catch (Exception $e) {
+                        echo $e;
+                        die();
+                    }
+            
+                    $studentGradeRow = mysqli_fetch_assoc($studentGradeResult);
+                    $studentGrade = $studentGradeRow['grade'];
+            
+                    // Verify student can be added to meeting
+                    if ($gradeReq > $studentGrade) {
+                        echo "Student does not meet grade requirement";
+                    } else {
+                        $addQuery = 'insert into enroll values (' .
+                            $meetingID . ', '. $studentID .')';
+                        try {
+                            $result = mysqli_query($dbConnection, $addQuery);
+                        } catch (mysqli_sql_exception $e) {
+                            echo $e;
+                        } finally {
+                            if ($result) {
+                                echo "<br><br>Student Added to meeting";
+                                redirect('studentJoinMeeting.php');
+                            }
+                        }
+                    }
                 }
             }
-            
+
+
+            // if(isset($_POST['delete'])) {
+            //     $meeting_id = $_POST['deletemeet'];
+            //     $student_id = $_SESSION['sessionID'];
+            //     if(empty($meeting_id)){
+            //         echo "Please enter meeting ID";
+            //     }
+            //     else if($meeting_id = $meetingID){
+            //         $deleteQuery = 'delete from enroll values (' . 
+            //             $meeting_id . ', '. $student_id. ')';
+            //             echo "Meeting has been successfully deleted.";
+            //             redirect('studentJoinMeeting.php');
+            //     }
+            //     else {
+            //         echo "No meetings added"; 
+            //     }
+            // }
             if(isset($_POST['delete'])) {
-                $meeting_id = $_POST['meeting'];
-                if(empty($meeting_id)){
-                    echo "Please enter meeting ID";
-                }
-                else if($meeting_id = $meetingID){
-                    $createQuery = 'delete from enroll values (' . 
-                        $meeting_id . ', '. $_SESSION['sessionID']. ')'; 
-                        $delete = mysqli_query($dbConnection, $createQuery);
-                        echo "Meeting has been successfully deleted.";
+                $meetingID = $_POST['deletemeet'];
+                $studentID = $_SESSION['sessionID'];
+                if (empty($meetingID)) {
+                    echo "<br><br>Input valid Meeting ID";
+                } else {
             
-                }
-                else {
-                    echo "No meetings added"; 
+                    // Query meeting desired for its associate group
+                    $validateGradeQuery = 'select * from meetings where meeting_id = ' . $meetingID;
+                    try {
+                        $validationResults = mysqli_query($dbConnection, $validateGradeQuery);
+                    } catch (mysqli_sql_exception $e) {
+                        echo $e;
+                        die();
+                    }
+                    $meetingCheckForValidation = mysqli_fetch_assoc($validationResults);
+                    $groupIDToCheckForValidation =  $meetingCheckForValidation['group_id'];
+            
+                    // Query associated group for its grade requirement
+                    $groupIDValidationQuery = 'select grade_req from groups where group_id = ' . $groupIDToCheckForValidation;
+                    try {
+                        $groupIDValidationResult = mysqli_query($dbConnection, $groupIDValidationQuery);
+                    } catch (mysqli_sql_exception $e) {
+                        echo $e;
+                        die();
+                    }
+            
+                    $gradeReqRow = mysqli_fetch_assoc($groupIDValidationResult);
+                    $gradeReq = $gradeReqRow['grade_req'];
+            
+                    // Query student for their grade
+                    $studentGradeQuery = 'select grade from students where student_id = ' . $studentID;
+                    try {
+                        $studentGradeResult = mysqli_query($dbConnection, $studentGradeQuery);
+                    } catch (Exception $e) {
+                        echo $e;
+                        die();
+                    }
+            
+                    $studentGradeRow = mysqli_fetch_assoc($studentGradeResult);
+                    $studentGrade = $studentGradeRow['grade'];
+            
+                    // Verify student can be added to meeting
+                    if ($gradeReq > $studentGrade) {
+                        echo "Student does not meet grade requirement";
+                    } else {
+                        $deleteQuery = 'delete from enroll values (' .
+                            $meetingID . ', '. $studentID .')';
+                        try {
+                            $result = mysqli_query($dbConnection, $deleteQuery);
+                        } catch (mysqli_sql_exception $e) {
+                            echo $e;
+                        } finally {
+                            if ($result) {
+                                echo "<br><br>Student Deleted to meeting";
+                                redirect('studentJoinMeeting.php');
+                            }
+                        }
+                    }
                 }
             }
+
 ?>
 </body>
 </html>
